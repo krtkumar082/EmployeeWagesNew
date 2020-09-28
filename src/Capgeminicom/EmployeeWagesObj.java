866a8db4e13bd7d0e6bc7d7cd82bc04d4eabf45c
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-09-28 12:09:25 +0530

    [Kirti]Refactored:Using Class Method

---
+++ src/Capgeminicom/EmployeeWagesObj.java
@@ -0,0 +1,12 @@
+package Capgeminicom;
+
+public class EmployeeWagesObj {
+	public static final int IS_PART_TIME =1;
+	public static final int IS_FULL_TIME =2;
+	private final String Company;
+	private  final int EMP_RATE_PER_HOUR;
+	private final int NUM_OF_WORKING_DAYS;
+	
+	  
+	EmployeeWagesObj(String Company,)
+}

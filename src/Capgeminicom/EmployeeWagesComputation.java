745e7ac28c32a372bc9b7e43e62e0e3356fa905b
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-09-28 12:28:34 +0530

    [Kirti]Refactored:For Multiple Companies

--- src/Capgeminicom/EmployeeWagesComputation.java
+++ src/Capgeminicom/EmployeeWagesComputation.java
@@ -2,41 +2,39 @@ package Capgeminicom;
 import java.util.Random;
 
 public class EmployeeWagesComputation {
-	public static final int IS_FULL_TIME = 2;
-	public static final int IS_PART_TIME = 1;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 
-	public static int computeEmpWage() {
-		int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-		totalWorkingDays++;
+	while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+	totalWorkingDays++;
 
-		Random r = new Random();
-		int empCheck = r.nextInt(3);
+	Random r = new Random();
+	int empCheck = r.nextInt(3);
 
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
+	switch (empCheck) {
+	case IS_FULL_TIME:
+	 empHrs = 8;
+	 break;
+	case IS_PART_TIME:
+	 empHrs = 4;
+	 break;
+	default:
+	 empHrs = 0;
 
-		}
-		totalEmpHrs += empHrs;
-		System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
-		}
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
-		return totalEmpWage;
+	}
+	totalEmpHrs += empHrs;
+	System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
+	}
+	totalEmpWage = totalEmpHrs * empRatePerHour;
+	System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
+	return totalEmpWage;
 	}
 	
 	public static void main(String[] args) {
-    computeEmpWage();
+     computeEmpWage("Reliance", 15, 25 , 100);
+     computeEmpWage("BigBazar", 20, 27 , 90);
 	}
 }
